db5827e62c19fcdf499e642fa1c8d5e0965e1c72
SickSect <sleepyboy906sb@gmail.com>
2022-05-21 21:14:42 +0300

    ph 12 feature

--- mandatory/thread_starting.c
+++ mandatory/thread_starting.c
@@ -48,6 +48,7 @@ static void	eating(t_philo *ph)
 	t_rules	*rule;
 
 	rule = ph->rules;
+	// need to add feature!! depend on biggest fork id
 	pthread_mutex_lock(&(rule->forks_mutex[ph->left_fork_id]));
 	print_action("has taken a fork", rule, ph->id);
 	pthread_mutex_lock(&(rule->forks_mutex[ph->right_fork_id]));
